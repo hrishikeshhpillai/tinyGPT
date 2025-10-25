44f76e4335ee54ac496060bf624d18b4f6f04029
hrishikeshhpillai <mail2hrishikeshhpillai@gmail.com>
2025-10-25 09:03:52 +0530

    add layer norm

--- bigram.py
+++ bigram.py
@@ -121,10 +121,12 @@ class Block(nn.Module):
         head_size = n_embed // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embed)
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
 
     def forward(self, x):
-        x = x + self.sa(x)
-        s = x + self.ffwd(x) # residual connections
+        x = x + self.sa(self.ln1(x)) # applying pre-norm
+        s = x + self.ffwd(self.ln2(x)) # residual connections
         return x
 
 
@@ -144,6 +146,7 @@ class BigramLanguageModel(nn.Module):
             Block(n_embed, n_head=4),
             Block(n_embed, n_head=4),
             Block(n_embed, n_head=4),
+            nn.LayerNorm(n_embed),
         )
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
