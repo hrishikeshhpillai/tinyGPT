980e42ececfcabcfe9afe87e072d3fa9c82a1383
hrishikeshhpillai <mail2hrishikeshhpillai@gmail.com>
2025-10-25 09:17:47 +0530

    add dropout, refactor

--- bigram.py
+++ bigram.py
@@ -5,14 +5,17 @@ from torch.nn import functional as F
 
 #hyperparameters
 
-batch_size = 32 # how many independent sequences will we process in parallel
-block_size = 8 # what is the maximum context length of predictions ?
+batch_size = 64 # how many independent sequences will we process in parallel
+block_size = 256 # what is the maximum context length of predictions ?
 max_iters = 5000
-eval_interval = 300
-learning_rate = 1e-3
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embed = 32
+n_embed = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 torch.manual_seed(1337)
 
@@ -66,6 +69,9 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embed, head_size, bias=False)
         self.value = nn.Linear(n_embed, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size))) #tril if not a parameter of module, so pytorch naming conventions buffer, we have to assign it to the module using the register_buffer
+
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x)     #(B, T, C)
@@ -75,6 +81,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1) * C**-0.5 # (B, T, C) @ (B, T, C) -> (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) #(B, T, T)
         wei = F.softmax(wei, dim=-1)# (B, T, T)
+        wei = self.dropout(wei)
         # perform the weighted aggregation of the values
         v = self.value(x) # (B, T, C)
         out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
@@ -88,10 +95,11 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embed, n_embed)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim = -1)
-        out = self.proj(out) # projection back into the residual pathway
+        out = self.dropout(self.proj(out)) # projection back into the residual pathway
         return out 
 
 
@@ -104,13 +112,14 @@ class FeedForward(nn.Module):
             nn.Linear(n_embed, 4 * n_embed), # 4* from the paper
             nn.ReLU(),
             nn.Linear(4 * n_embed, n_embed), # projetion layer going back into the residual pathway
+            nn.Dropout(dropout)
 
         )
 
     def forward(self, x):
         return self.net(x) # on a per token level, all tokens do this independently (self attention is the communication and once it has gathered he data they need to think on that data individually)
 
-
+ 
 class Block(nn.Module):
     """" Transformer block: communication followed by computation """
 
@@ -142,12 +151,14 @@ class BigramLanguageModel(nn.Module):
 
         # this won't give a very good results, as we are getting towards a very deep neural net, and deep neural nets suffer from optimisation issues
         # solution: 1. Residual Connection, 2. Layer Norm
-        self.blocks = nn.Sequential(
-            Block(n_embed, n_head=4),
-            Block(n_embed, n_head=4),
-            Block(n_embed, n_head=4),
-            nn.LayerNorm(n_embed),
-        )
+        # self.blocks = nn.Sequential(
+        #     Block(n_embed, n_head=4),
+        #     Block(n_embed, n_head=4),
+        #     Block(n_embed, n_head=4),
+        #     nn.LayerNorm(n_embed),
+        # )
+        self.blocks = nn.Sequential(*[Block(n_embed, n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embed) # final layer norm
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -161,6 +172,7 @@ class BigramLanguageModel(nn.Module):
         # x = self.sa_heads(x)
         # x = self.ffwd(x)
         x = self.blocks(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x) #(B, T, vocab_size)
         if targets is None:
             loss = None
